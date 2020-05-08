408d78e866e8881d5c914ad7a367b78f53079bfa
Manoj <manoj@manojs-MacBook-Pro.local>
2020-05-08 20:09:44 +0530

    feedback factor change

--- sourcecode/train.py
+++ sourcecode/train.py
@@ -103,7 +103,7 @@ def parse_arguments():
                         help="number of epochs for training")
 
     parser.add_argument("--feedback_factor", action="store", type=int,
-                        default=100,
+                        default=1000,
                         help="number of logs to generate per epoch")
 
     parser.add_argument("--num_samples", action="store", type=int,
